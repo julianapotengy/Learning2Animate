75a4694a85a9e73f19b6664bba06351d67560862
Juliana Potengy <jupotengy@gmail.com>
2016-11-28 22:51:29 -0200

    First commit
    I'm trying to learn how to use animator in Unity to animate separate
    body parts

---
+++ Learning2Animate/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	private float axisX, axisY, speed;
+	private Rigidbody2D body;
+	private Animator anim;
+
+	void Start ()
+	{
+		speed = 10 * Time.deltaTime;
+		body = this.GetComponent<Rigidbody2D> ();
+		anim = this.GetComponent<Animator>();
+	}
+
+	void FixedUpdate()
+	{
+		float move = Input.GetAxis("Horizontal");
+		anim.SetFloat("speed", Mathf.Abs(move));
+	}
+
+	void Update ()
+	{
+		axisX = Input.GetAxis ("Horizontal");
+		axisY = Input.GetAxis ("Vertical");
+		body.velocity = new Vector2(axisX * speed, axisY);
+	}
+}
